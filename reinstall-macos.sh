31f4d6aea708a2b517b757fe9316bfdb64a0323e
Dmitry <digy@digy.ru>
2022-12-09 03:24:33 +0100

    daily

--- reinstall-macos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/zzzamyatin/environment/HEAD/reinstall-macos.sh)"
-#
-
-echo 1
---
+++ reinstall-macos/reinstall-macos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/zzzamyatin/environment/HEAD/reinstall-macos/reinstall-macos.sh)"
+# 
+
+
+echo 1
